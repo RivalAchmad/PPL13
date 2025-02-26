8726b8128379fbeb79b0abb99ec8737951ed901d
RivalAchmad <159166059+RivalAchmad@users.noreply.github.com>
2025-02-26 12:14:26 +0700

    Update hello_aut.py

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -2,27 +2,28 @@ import unittest, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class GoogleTestCase(unittest.TestCase):
+class AutTest(unittest.TestCase):
+
     def setUp(self):
         options = webdriver.FirefoxOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
-        options.add_argument('--ignore-ssl-errors')
-        server = 'http://localhost:4444'
+        server = "http://localhost:4444"
 
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
-    def test_Homepage(self):
+    def test_homepage(self):
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
             url = "http://localhost"
-        
+
         self.browser.get(url)
-        self.browser.save_screenshot('screenshot.png')
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
+
         self.assertIn(expected_result, actual_result.text)
 
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'],verbosity=2, warnings='ignore')
+if __name__ == "__main__":
+    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
